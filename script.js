5e7674394894950d73ad2f71f4794fb531a3e6e7
Dinesh kumar <dinneshkumar9@gmail.com>
2023-03-22 14:47:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,27 @@
-const calculateTotalBtn = document.getElementById('calculate-total-btn');
-		calculateTotalBtn.addEventListener('click', calculateTotal);
+// Get all the price elements
+const priceElements = document.querySelectorAll('[data-ns-test="prices"]');
 
-		function calculateTotal() {
-			const prices = document.querySelectorAll('.price');
-			let totalPrice = 0;
-			prices.forEach(price => {
-				totalPrice += parseFloat(price.textContent);
-			});
-			const totalPriceCell = document.getElementById('ans');
-			totalPriceCell.textContent = totalPrice.toFixed(2);
-		}
+// Check the number of price elements
+if (priceElements.length === 0) {
+  console.error('No price elements found');
+} else {
+  console.log(`Found ${priceElements.length} price elements`);
+
+  // Calculate the total price
+  let totalPrice = 0;
+  priceElements.forEach(priceElement => {
+    totalPrice += parseFloat(priceElement.textContent);
+  });
+
+  // Add the total price to the table
+  const grandTotalElement = document.querySelector('[data-ns-test="grandTotal"]');
+  grandTotalElement.textContent = totalPrice.toFixed(2);
+
+  // Check if the grand total matches the sum of prices
+  const expectedGrandTotal = priceElements.length > 0 ? totalPrice.toFixed(2) : '';
+  if (grandTotalElement.textContent !== expectedGrandTotal) {
+    console.error(`Grand total (${grandTotalElement.textContent}) does not match sum of prices (${expectedGrandTotal})`);
+  } else {
+    console.log(`Grand total (${grandTotalElement.textContent}) matches sum of prices (${expectedGrandTotal})`);
+  }
+}
