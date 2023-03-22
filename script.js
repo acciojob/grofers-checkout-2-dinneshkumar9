49b2673422c3fd8be428808a1b033fa7340a12f9
Dinesh kumar <dinneshkumar9@gmail.com>
2023-03-22 12:32:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,12 +2,27 @@
 // Get all the price elements
 const priceElements = document.querySelectorAll('[data-ns-test="prices"]');
 
-// Calculate the total price
-let totalPrice = 0;
-priceElements.forEach(priceElement => {
-  totalPrice += parseFloat(priceElement.textContent);
-});
+// Check the number of price elements
+if (priceElements.length === 0) {
+  console.error('No price elements found');
+} else {
+  console.log(`Found ${priceElements.length} price elements`);
 
-// Add the total price to the table
-const grandTotalElement = document.querySelector('[data-ns-test="grandTotal"]');
-grandTotalElement.textContent = totalPrice.toFixed(2);
+  // Calculate the total price
+  let totalPrice = 0;
+  priceElements.forEach(priceElement => {
+    totalPrice += parseFloat(priceElement.textContent);
+  });
+
+  // Add the total price to the table
+  const grandTotalElement = document.querySelector('[data-ns-test="grandTotal"]');
+  grandTotalElement.textContent = totalPrice.toFixed(2);
+
+  // Check if the grand total matches the sum of prices
+  const expectedGrandTotal = priceElements.length > 0 ? totalPrice.toFixed(2) : '';
+  if (grandTotalElement.textContent !== expectedGrandTotal) {
+    console.error(`Grand total (${grandTotalElement.textContent}) does not match sum of prices (${expectedGrandTotal})`);
+  } else {
+    console.log(`Grand total (${grandTotalElement.textContent}) matches sum of prices (${expectedGrandTotal})`);
+  }
+}
