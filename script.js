7a004d85490777cab28f659e18c645c6de844463
Dinesh kumar <dinneshkumar9@gmail.com>
2023-03-22 12:21:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your code here
+// Get all the price elements
+const priceElements = document.querySelectorAll('[data-ns-test="prices"]');
+
+// Calculate the total price
+let totalPrice = 0;
+priceElements.forEach(priceElement => {
+  totalPrice += parseFloat(priceElement.textContent);
+});
+
+// Add the total price to the table
+const grandTotalElement = document.querySelector('[data-ns-test="grandTotal"]');
+grandTotalElement.textContent = totalPrice.toFixed(2);
